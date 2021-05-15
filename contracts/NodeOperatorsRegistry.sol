052284744f772b6509baed0e025f843bca7a516a
Tanishq Jasoria <jasoriatanishq@gmail.com>
2021-05-15 17:21:43 +0530

    update functions

--- contracts/NodeOperatorsRegistry.sol
+++ contracts/NodeOperatorsRegistry.sol
@@ -23,32 +23,36 @@ contract NodeOperatorsRegistry {
 
     uint256 public approvedKeys;
     uint256 public usedKeys;
+    // adding here for after keys not stored in contract
+    uint256 public totalKeysCount;
     string public ipfsHash;
     string public merkelRoot;
 
     constructor(address _nodeOperator, address _governance) {
         nodeOperator = _nodeOperator;
         governance = _governance;
+        totalKeysCount = 0;
     }
 
     function totalKeys() public view returns (uint256) {
         return keys.length;
     }
 
-    function getIPFSHashMerkelRoot() public view returns (string memory, string memory) {
+    function getOperatorDetails() public view returns (string memory, string memory) {
         return (ipfsHash, merkelRoot);
 
+    }
+
+    function updateOperatorDetails(string memory _ipfsHash, string memory _merkelRoot, uint256 _newKeysCount) external onlyNodeOperator {
+        // add new pubkey and signature
+        ipfsHash = _ipfsHash;
+        merkelRoot = _merkelRoot;
+        totalKeysCount = totalKeysCount + _newKeysCount;
     }
 
     function addKey(bytes memory _pubkey, bytes memory _signature) external onlyNodeOperator {
         // add new pubkey and signature
         keys.push(PubkeyAndSignature(_pubkey, _signature));
-    }
-
-    function addIPFSHashMerkelRoot(string memory _ipfsHash, string memory _merkelRoot) external onlyNodeOperator {
-        // add new pubkey and signature
-        ipfsHash = _ipfsHash;
-        merkelRoot = _merkelRoot;
     }
 
     function approveKeys(uint256 newApprovedKeys) external onlyGovernance {
